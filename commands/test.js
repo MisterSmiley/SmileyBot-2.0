78292035a992f88958a9f73cc7189fbe88fb185e
MisterSmiley <35432693+MisterSmiley@users.noreply.github.com>
2018-04-16 13:29:18 +0200

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,6 +1,9 @@
 const config = require('../config/config.json');
+const Discord = require('discord.js')
 exports.run = async (client, msg, args) => {
-    msg.channel.send("<:annonces:403632327328268301>");
+    var test = new Discord.RichEmbed()
+    .addField("test", "<:annonces:403632327328268301>")
+    msg.channel.send(test)
 };
 exports.help = {
     category: 'devonly',
