0bf0bd837ee920a14eca41dc3233ab26309a1b55
MisterSmiley <35432693+MisterSmiley@users.noreply.github.com>
2018-04-24 19:13:03 +0200

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,10 +1,39 @@
 const config = require('../config/config.json');
 const Discord = require('discord.js')
 exports.run = async (client, msg, args) => {
-    var test = new Discord.RichEmbed()
-    .addField("test", "<:annonces:403632327328268301>")
-    msg.channel.send(test)
-};
+    let pages = ["Test1", "Test2"]
+    let page = 1;
+    const embed = new Discord.MessageEmbed()
+        .setColor(0xffffff)
+        .setFooter(`Page ${page} of ${pages.length}`)
+        .setDescription(pages[page - 1])
+
+    message.channel.send(embed).then(msg => {
+
+        msg.react('⏪').then(r => { msg.react('⏩')
+            
+            const backwards = msg.createReactionCollector((reaction, user) => reaction.emoji.name === '⏪' && user.id === message.author.id, { time: 60000 });
+            const forwards = msg.createReactionCollector((reaction, user) => reaction.emoji.name === '⏩' && user.id === message.author.id, { time: 60000 });
+
+            function reset() {
+                embed.setDescription(pages[page - 1]);
+                embed.setFooter(`Page ${page} of ${pages.length}`);
+                msg.edit(embed)
+            }
+
+            backwards.on('collect', r => {
+                if (page === 1) return;
+                page--;
+                reset()
+            })
+
+            forwards.on('collect', r => {
+                if (page === pages.length) return;
+                page++;
+                reset()
+            })
+        })
+    })
 exports.help = {
     category: 'devonly',
     usage: '[devonly]',
