e925184bf8d20e2289beb83252a60f0debb0d1e5
MisterSmiley <35432693+MisterSmiley@users.noreply.github.com>
2018-04-28 15:06:53 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 exports.run = async client => {
     logger.debug(`Logged in as ${client.user.tag}`);
     logger.debug(`Serving ${client.guilds.size} servers with ${client.users.filter(i => !i.bot).size} users`);
-    client.user.setPresence({game: {name: '&&help | Tests', type: 0}}).catch(err => logger.error(err));
+    client.user.setPresence({game: {name: '&help | by Mister Smiley#6699', type: 0}}).catch(err => logger.error(err));
 
 };
