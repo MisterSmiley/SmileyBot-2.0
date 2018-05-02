bfe68d5c335d68ebd457f81b5152933f79ffc1c3
MisterSmiley <35432693+MisterSmiley@users.noreply.github.com>
2018-05-02 22:42:57 +0200

    Create paladins.js

---
+++ commands/paladins.js
@@ -0,0 +1,28 @@
+const Hirez = require('hirez.js')
+ 
+let hirez = new Hirez({
+  devId: '2586',
+  authKey: 'F834134C87134905A5C0C07877C1702F'
+})
+
+exports.run = async (client, msg, args) => {
+    hirez.paladins('PC').session.generate()
+  .then((res) => {
+})
+hirez.paladins('PC').getMatchHistory('AlexGames360')
+  .then((res) => {
+  console.log(res)
+  })
+  
+  exports.help = {
+    category: 'DeveloperOnly',
+    usage: 'test',
+    description: 'test',
+    detail: 'test',
+    botPerm: ['SEND_MESSAGES', 'EMBED_LINKS'],
+    authorPerm: [],
+    example: 'test',
+    alias: [
+        null
+    ]
+};
