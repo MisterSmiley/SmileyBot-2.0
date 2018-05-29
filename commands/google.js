7d7f04345afe2929979e18f6edcd4dbd96e5fec3
MisterSmiley <35432693+MisterSmiley@users.noreply.github.com>
2018-05-29 02:11:30 +0200

    Update google.js

--- commands/google.js
+++ commands/google.js
@@ -12,13 +12,12 @@ exports.run = (client, msg, args) => {
         for (var i = 0; i < 1; ++i) {
     var link = res.links[i];
             
-        await msg.react('<:google:450812327118241793>');
         let embed = new Discord.RichEmbed()
         .setThumbnail("https://cdn.discordapp.com/attachments/413814161395548162/450812278346874881/2000px-Google__G__Logo.svg.png")
         .setColor("#c20d0d")
         .setTimestamp()
         .setTitle("SmileyBot")
-        .addField("**<:google:450812327118241793> I found**", `[${link.title}](${link.href}) \n\n${link.description}`)
+        .addField("**<:google:450812327118241793> | Founded !**", `[${link.title}](${link.href}) \n\n${link.description}`)
         await msg.channel.send(embed);
         }
     });
